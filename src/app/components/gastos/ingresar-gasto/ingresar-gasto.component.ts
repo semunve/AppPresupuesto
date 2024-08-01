823ce3d130872a83943a1000a03cee05fee1723b
semunve <smunve@gmail.com>
2024-08-01 19:05:08 +0200

    add ingreso gasto

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PresupuestoService } from '../../../services/presupuesto.service';
 
 @Component({
   selector: 'app-ingresar-gasto',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrl: './ingresar-gasto.component.css'
 })
 export class IngresarGastoComponent {
+
+  nombreGasto:string;
+  cantidad:number;
+  formularioIncorrecto:boolean;
+  textoIncorrecto:string;
+
+  /**
+   *
+   */
+  constructor(private _presupuestoService:PresupuestoService) {
+   this.nombreGasto='';
+   this.cantidad=0;
+   this.formularioIncorrecto=false;
+   this.textoIncorrecto='';
+    
+  }
+  agregarGasto(){
+    if(this.cantidad > this._presupuestoService.restante){
+      this.formularioIncorrecto=true;
+      this.textoIncorrecto='Cantidad mayor que la restante';
+      return;
+    }
+    const GASTO={
+      nombre:this.nombreGasto,
+      cantidad:this.cantidad
+    }
+
+    this._presupuestoService.agregarGasto(GASTO)
+
+    if(this.nombreGasto=='' || this.cantidad<=0){
+      this.formularioIncorrecto=true;
+      
+    }else{
+      this.formularioIncorrecto=false;
+      this.nombreGasto='';
+      this.cantidad=0;
+    }
+  }
 
 }
